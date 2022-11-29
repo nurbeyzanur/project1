05b280de2c37b06ef7eb2fbe0c825c65a0008eb3
nurbeyzanur <117708453+nurbeyzanur@users.noreply.github.com>
2022-11-29 14:41:53 +0300

    Update proje2.java

--- proje2.java
+++ proje2.java
@@ -1,18 +1,19 @@
 
 package algoritmaprojeleri;
-
+//Derece cinsinden girilen sayıyı radyan ve grad birimine çeviren program
 import java.util.Scanner;
 
 public class proje2 {
     public static void main(String[] args) {
         Scanner scan =new Scanner(System.in);
-       double a,radyan,gradyan;
+       double a,radyan,grad;
         
         System.out.println("Lütfen derece şeklinde açıyı giriniz:");
-        a =scan.nextInt();        
-        radyan=a*Math.PI/180;
-        System.out.println("Radyan"+radyan);
-        
+        a = scan.nextInt();        
+        radyan = a*Math.PI/180;
+        System.out.println("Radyan: "+radyan);
+        grad = a*10/9;
+        System.out.println("Grad: "+grad);
         
     }
 }
